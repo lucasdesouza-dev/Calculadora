295209376911148a9a130ab1ad26bf369540741e
lusousouza <luca.s.ouza@hotmail.com>
2022-06-30 12:45:56 -0300

    adicionando funcoes e corrigindo cores

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import type { TSimbolo } from "./Calculadora";
 interface Props {
-  data: (number | string | undefined)[];
+  data: TSimbolo;
   active: boolean;
+  largo?: boolean;
+  gray: boolean;
+
+  funcao: () => void;
 }
-export function Button({ data, active }: Props) {
+
+export function Button({
+  data,
+  active,
+  largo,
+  gray,
+
+  funcao,
+}: Props) {
+  const clickHandler: React.MouseEventHandler<HTMLButtonElement> = () => {
+    if (funcao != null) {
+      funcao();
+    }
+
+    // if (simbolo === "AC" && AC != null) {
+    //   AC();
+    // }
+    // if (simbolo === "+/-" && maisMenos != null) {
+    //   maisMenos();
+    // }
+  };
   return (
     <div>
-      <button className={active ? "color" : "gray"}>{data}</button>
+      <button
+        onClick={clickHandler}
+        className={`${active ? "color" : "darkGray"} ${gray ? "gray" : ""} ${
+          largo != null && largo ? " buttonLargo" : ""
+        }`}
+        value={data}
+      >
+        {data}
+      </button>
     </div>
   );
 }
--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,48 +1,172 @@
 import "../styles/Calculadora.css";
 import { Button } from "./Button";
+import { useState } from "react";
+
+export type TSimbolo = number | string;
+const simbolos = [
+  "AC",
+  "+/-",
+  "%",
+  "/",
+  7,
+  8,
+  9,
+  "x",
+  4,
+  5,
+  6,
+  "-",
+  1,
+  2,
+  3,
+  "+",
+  "0",
+  ",",
+  "=",
+] as TSimbolo[];
 
 export function Calculadora() {
-  const numeros = [
-    "AC",
-    "+/-",
-    "%",
-    "/",
-    7,
-    8,
-    9,
-    ,
-    "x",
-    4,
-    5,
-    6,
-    "-",
-    1,
-    2,
-    3,
-    "+",
-    "0",
-    "",
-    ",",
-    "=",
-  ];
+  const [oldResultado, setOldResultado] = useState<any | string | number>(0);
+  const [resultado, setResultado] = useState<any | string | number>(0);
+  const [operador, setOperador] = useState<any | string>("");
+  const funcoesSimbolos: { [id: string]: () => void } = {
+    AC: () => {
+      setResultado(0);
+      setOldResultado(0);
+    },
+    "+/-": () => {
+      setResultado(-resultado);
+    },
+    "%": () => {
+      setResultado(resultado / 100);
+    },
+    "/": () => {
+      setOperador("/");
+      setOldResultado(resultado);
+      setResultado(0);
+    },
+    x: () => {
+      setOperador("x");
+      setOldResultado(resultado);
+      setResultado(0);
+    },
+    "-": () => {
+      setOperador("-");
+      setOldResultado(resultado);
+      setResultado(0);
+    },
+    "+": () => {
+      setOperador("+");
+      setOldResultado(resultado);
+      setResultado(0);
+    },
+    "=": () => {
+      if (operador === "/") {
+        setResultado(oldResultado / resultado);
+      } else if (operador === "x") {
+        setResultado(oldResultado * resultado);
+      } else if (operador === "-") {
+        setResultado(oldResultado - resultado);
+      } else if (operador === "+") {
+        setResultado(parseFloat(oldResultado) + parseFloat(resultado));
+      }
+    },
+    ",": () => {
+      console.log(",");
+    },
+    "0": () => {
+      if (resultado === 0) {
+        setResultado(0);
+      } else {
+        setResultado(resultado + "0");
+      }
+    },
+    "1": () => {
+      if (resultado === 0) {
+        setResultado(1);
+      } else {
+        setResultado(resultado + "1");
+      }
+    },
+    "2": () => {
+      if (resultado === 0) {
+        setResultado(2);
+      } else {
+        setResultado(resultado + "2");
+      }
+    },
+    "3": () => {
+      if (resultado === 0) {
+        setResultado(3);
+      } else {
+        setResultado(resultado + "3");
+      }
+    },
+    "4": () => {
+      if (resultado === 0) {
+        setResultado(4);
+      } else {
+        setResultado(resultado + "4");
+      }
+    },
+    "5": () => {
+      if (resultado === 0) {
+        setResultado(5);
+      } else {
+        setResultado(resultado + "5");
+      }
+    },
+    "6": () => {
+      if (resultado === 0) {
+        setResultado(6);
+      } else {
+        setResultado(resultado + "6");
+      }
+    },
+    "7": () => {
+      if (resultado === 0) {
+        setResultado(7);
+      } else {
+        setResultado(resultado + "7");
+      }
+    },
+    "8": () => {
+      if (resultado === 0) {
+        setResultado(8);
+      } else {
+        setResultado(resultado + "8");
+      }
+    },
+    "9": () => {
+      if (resultado === 0) {
+        setResultado(9);
+      } else {
+        setResultado(resultado + "9");
+      }
+    },
+  };
 
   return (
     <div className="corpo">
       <div className="visor">
-        <strong>124586</strong>
+        <strong>{resultado}</strong>
       </div>
       <div className="buttons ">
-        {numeros.map((numero) => {
+        {simbolos.map((simbolo) => {
           return (
             <Button
+              key={simbolo}
+              funcao={funcoesSimbolos[simbolo]}
               active={
-                numero === "/" ||
-                numero === "x" ||
-                numero === "-" ||
-                numero === "+" ||
-                numero === "="
+                simbolo === "/" ||
+                simbolo === "x" ||
+                simbolo === "-" ||
+                simbolo === "+" ||
+                simbolo === "="
               }
-              data={numero}
+              gray={simbolo === "AC" || simbolo === "+/-" || simbolo === "%"}
+              largo={simbolo === "0"}
+              data={simbolo}
             />
           );
         })}
